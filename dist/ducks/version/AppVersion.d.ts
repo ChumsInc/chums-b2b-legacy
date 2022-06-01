9f68207390f200e7e9d096a7a1b589c100cd142d
Steve Montgomery <steve@chums.com>
2022-06-01 11:54:34 -0600

    fix: remove unused dist path

--- dist/ducks/version/AppVersion.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-declare const AppVersion: React.FC;
-export default AppVersion;
--- dist/ducks/version/actions.d.ts
+++
@@ -1,3 +0,0 @@
-import { ScreenThunkAction, VersionAction } from "./index";
-export declare const fetchVersion: (force?: boolean | undefined) => ScreenThunkAction;
-export declare const ignoreVersion: () => VersionAction;
