ed1ad27542cff315f4e79c84648325f28f43a6b6
Steve Montgomery <steve@chums.com>
2022-06-07 08:58:53 -0600

    fix: remove variant dropdown if there is only one active variant.

--- src/components/VariantSelector.js
+++ src/components/VariantSelector.js
@@ -1,9 +1,9 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 // import {connect} from 'react-redux';
 import ClickOutside from 'react-onclickoutside';
 import classNames from "classnames";
-import {sortVariants, getMSRP, getPrices, getSalesUM} from "../utils/products";
+import {getMSRP, getPrices, getSalesUM, sortVariants} from "../utils/products";
 import numeral from 'numeral';
 
 const VariantItem = ({variant, selected = false, loggedIn = false, onClick, priceCodes = []}) => {
@@ -86,16 +86,36 @@ class VariantSelector extends Component {
             : getMSRP(variant.product);
         const salesUM = getSalesUM(variant.product);
 
+        if (variants.filter(v => !!v.status && !!v.product.status).length <= 1) {
+            return (
+                <div className="variants-container">
+                    <div className="btn btn-outline-secondary variant-selector">
+                        <div>
+                            <span className="title"
+                                  dangerouslySetInnerHTML={{__html: variant.title || 'Select an option'}}/>
+
+                            <span className="price ms-3">
+                                $ {prices.map(price => numeral(price).format('0.00')).join(' - ')}
+                                {' '}
+                                ({salesUM || 'EA'})
+                            </span>
+                        </div>
+                        {/*{variant.title || 'Select an Option'}*/}
+                    </div>
+                </div>
+            )
+        }
         return (
             <div className="variants-container dropdown">
                 <div className={classNames("btn-group", {show})}>
-                    <button type="button"
+                    <button type="button" disabled={variants.filter(v => !!v.status && !!v.product.status).length <= 1}
                             className="btn btn-outline-secondary dropdown-toggle variant-selector"
                             onClick={this.onClickSelector}>
                         <div>
-                            <span className="title" dangerouslySetInnerHTML={{__html: variant.title || 'Select an option'}}/>
+                            <span className="title"
+                                  dangerouslySetInnerHTML={{__html: variant.title || 'Select an option'}}/>
 
-                            <span className="price ml-3">
+                            <span className="price ms-3">
                                 $ {prices.map(price => numeral(price).format('0.00')).join(' - ')}
                                 {' '}
                                 ({salesUM || 'EA'})
