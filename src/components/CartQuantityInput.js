dfd454661d991e04b2b0b7b8283d488c506ec489
Steve Montgomery <steve@chums.com>
2023-03-17 11:30:22 -0600

    feat(CartQtyInput): make it more mobile friendly

--- src/components/CartQuantityInput.js
+++ src/components/CartQuantityInput.js
@@ -6,20 +6,18 @@ const CartQuantityInput = ({quantity, onChange, onAddToCart, min = 1, disabled =
         <div className="row g-3">
             <div className="col">
                 <div className="input-group number-input-group cart-qty-input">
+                    <button type="button" className="btn btn-sm btn-outline-secondary"
+                            title="decrement"
+                            onClick={() => onChange(quantity - 1)} disabled={quantity <= min}>
+                        <span className="bi-caret-down-fill"/>
+                    </button>
                     <TextInput type="number" pattern="[1-9][0-9]*" onChange={({value}) => onChange(value)}
                                required value={quantity || (min === 0 ? 0 : '')} min={min}/>
-                    <div className="row g-0 cart-qty--buttons">
-                        <button type="button" className="btn btn-sm btn-outline-secondary"
-                                title="increment"
-                                onClick={() => onChange(quantity + 1)}>
-                            <span className="bi-caret-up-fill" />
-                        </button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary"
-                                title="decrement"
-                                onClick={() => onChange(quantity - 1)} disabled={quantity <= min}>
-                            <span className="bi-caret-down-fill"/>
-                        </button>
-                    </div>
+                    <button type="button" className="btn btn-sm btn-outline-secondary"
+                            title="increment"
+                            onClick={() => onChange(quantity + 1)}>
+                        <span className="bi-caret-up-fill" />
+                    </button>
                 </div>
             </div>
             <div className="col-auto">
